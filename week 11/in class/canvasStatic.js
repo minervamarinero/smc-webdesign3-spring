93fa85fcf334272706ae4f9c97fb60dc1a1cc971
Minerva Marinero <marinero_minerva_car@student.smc.edu>
2021-05-14 19:05:17 -0700

    week 11 homework

---
+++ week 11/in class/canvasStatic.js
@@ -0,0 +1,33 @@
+const canvas1 = document.getElementById("staticCanvas")
+const cntx = canvas1.getContext('2d');
+
+
+/* allows you st the fill color*/ 
+cntx.fillStyle = "darkblue";
+cntx.fillRect(20, 140, 200, 300);
+
+
+cntx.fillStyle = "orange";
+
+cntx.beginPath();
+cntx.moveTo(200, 75); //x, then y
+cntx.lineTo(350, 200);
+cntx.lineTo(150, 100);
+cntx.lineTo(200, 75);
+cntx.fill();
+cntx.closePath();
+
+
+/*  measurements of arc are not in degree but radian 
+(meaning how many times the radians fill in the circumference of the circle 2piexradains=circumference)  */
+cntx.beginPath();
+cntx.fillStyle = "teal";
+cntx.arc(500, 300, 100, 0, Math.PI*2/3, true) //x, y, radius, start angle, end angle
+cntx.fill();
+cntx.closePath();
+
+cntx.strokeStyle = "red";
+cntx.font = "49px Arial, Helvetica, sans-serif";
+cntx.textAlign = "center";
+cntx.strokeText("Text on Canvas!", 400, 400);
+
